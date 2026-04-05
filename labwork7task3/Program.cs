e5b8ba2d99a16249a1bd1f074d5f2b576cac041d
monynetsvlad-creator <monynetsvlad@gmail.com>
2026-04-05 22:30:37 +0300

    перший коміт

---
+++ labwork7task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Labwork7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            Func<double, double> discountCalculator = null;
+            
+            discountCalculator += price => price * 0.95;  // -5%
+            discountCalculator += price => price * 0.90;  // -10%
+            discountCalculator += price => price - 100;   // -100 грн
+
+            double initialPrice = 1000;
+            
+            double result = discountCalculator(initialPrice);
+
+            Console.WriteLine($"Початкова ціна: {initialPrice}");
+            Console.WriteLine($"Результат стандартного виклику: {result} (Очікувано 900)");
+        }
+    }
+}
